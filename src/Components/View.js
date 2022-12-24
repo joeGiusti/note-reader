e2ccfeb856d16884c7b07822fd75f246e940d0a4
Joe Giusti <joedgiusti@gmail.com>
2022-12-24 02:18:01 -0500

    added deploy scripts to package.json

--- src/Components/View.js
+++ src/Components/View.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
+import {nouns} from "../Nouns.js"
 
 function View(props) {
   
@@ -7,7 +8,10 @@ function View(props) {
     const lineRef = useRef(0)
     const [lineState, setLineState] = useState(0)
     const timeoutRef = useRef()
-    const justPaused = useRef(false)
+    const justPaused = useRef()
+    const wordArray = useRef([])
+    const numberArray = useRef([])
+    const nounsArray = useRef([])
 
     useEffect(()=>{
         
@@ -18,10 +22,93 @@ function View(props) {
             console.log(event)
         })
 
+        nounsArray.current = nouns.split("\n")
+
     },[])
         
+    function parseLine(_line){
+        var tempLine = _line
+
+        console.log("parsing line "+tempLine)
+        console.log('tempLine.includes("<word>")'+tempLine.includes("<word>"))
+        console.log('tempLine.includes("<number>")'+tempLine.includes("<number>"))
+        console.log('tempLine.includes("<number26>")'+tempLine.includes("<number26>"))
+
+        // While line contains <word> and itterations < x
+        var c = 0
+        while(tempLine.includes("<word>") && (c < 10)){        
+            console.log("templine includes word")    
+            tempLine = tempLine.replace("<word>", randomWord())            
+            c++
+        }
+       
+        tempLine = tempLine.replace("<word-0>", wordArray.current[0])
+        tempLine = tempLine.replace("<word-1>", wordArray.current[1])
+        tempLine = tempLine.replace("<word-2>", wordArray.current[2])
+        tempLine = tempLine.replace("<word-3>", wordArray.current[3])
+
+        // While line contains <word> and itterations < x
+        c = 0
+        while(tempLine.includes("<number>") && (c < 10)){            
+                tempLine = tempLine.replace("<number>", randomNumber())
+                console.log("placed a number")
+            c++
+        }
+        c = 0
+        while(tempLine.includes("<number26>") && (c < 10)){            
+            tempLine = tempLine.replace("<number26>", randomNumber26())
+            console.log("placed a number within 26")
+            c++
+        }
+        try{tempLine = tempLine.replace("<number-0>", numberArray.current[0])}catch{}
+        try{tempLine = tempLine.replace("<number-1>", numberArray.current[1])}catch{}
+        try{tempLine = tempLine.replace("<number-2>", numberArray.current[2])}catch{}
+        try{tempLine = tempLine.replace("<number-3>", numberArray.current[3])}catch{}                        
+
+        console.log("parsed line: "+tempLine)
+
+        return tempLine
+    }
+    function randomWord(){
+
+        // Generate a new word
+        const wordIndex = Math.floor(Math.random() * nounsArray.current.length)
+
+        // 100 is the number of words in the array newWord = wordArray[wordIndex]
+        //const newWord = "duck"
+        const newWord = nounsArray.current[wordIndex]
+
+        // Put it a register array so it can be accessed later
+        //wordArray.current = [newWord, ...wordArray.current]
+        wordArray.current.push(newWord)
+        console.log("word array is currently: ")
+        console.log(wordArray.current)
+
+        // Return it to be used
+        return newWord
+    }
+    function randomNumber(){
+        // Generate a new random number between 1 and 100
+        const newNumber = Math.floor((Math.random() * 100) + 1)
+
+        // Put it a register array so it can be accessed later
+        numberArray.current.push(newNumber)
+
+        // Return it to be used
+        return newNumber
+    }
+    function randomNumber26(){
+        // Generate a new random number between 1 and 100
+        const newNumber = Math.floor((Math.random() * 26) + 1)
+
+        // Put it a register array so it can be accessed later
+        numberArray.current.push(newNumber)
+
+        // Return it to be used
+        return newNumber
+    }
     function readNextLine(){
-        
+        console.log("reading line")
         // If there is no note array return
         if(!Array.isArray(noteArray))
             return
@@ -37,9 +124,9 @@ function View(props) {
 
         }
         else{
-            
+
             // Speak the current line after a pause
-            speak(noteArray[lineRef.current]).then(()=>{
+            speak(parseLine(noteArray[lineRef.current])).then(()=>{
                 setTimeout(() => {
                     readNextLine()
                 }, 2000); 
